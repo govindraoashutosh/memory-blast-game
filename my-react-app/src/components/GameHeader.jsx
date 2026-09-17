993fc3ca982dd33261666f7ba1d6260f59c6865c
Ashutosh Govind Rao <govindraoashutosh@gmail.com>
2026-09-17 17:36:10 +0530

    added gameHeader

---
+++ my-react-app/src/components/GameHeader.jsx
@@ -0,0 +1,10 @@
+function GameHeader() {
+  return (
+    <header className="game-header">
+      <h1>Memory Blast</h1>
+      <p>Match all 5 pairs. Watch out for 💣 and ⏱️.</p>
+    </header>
+  );
+}
+
+export default GameHeader;
---
+++ my-react-app/src/components/MemoryGame.jsx
@@ -0,0 +1,11 @@
+import GameHeader from "./GameHeader";
+
+function MemoryGame() {
+  return (
+    <div className="memory-game">
+      <GameHeader />
+    </div>
+  );
+}
+
+export default MemoryGame;
